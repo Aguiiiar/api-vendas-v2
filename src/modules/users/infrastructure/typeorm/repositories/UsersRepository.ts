38bf9f37c4efb546780805509f4babd0dbad8b86
Aguiiiar <devaguiar14@gmail.com>
2022-10-02 00:50:04 -0300

    feat: implement user repository

--- src/modules/users/infrastructure/typeorm/repositories/UsersRepository.ts
+++ src/modules/users/infrastructure/typeorm/repositories/UsersRepository.ts
@@ -1,11 +1,54 @@
+import { ICreateUser } from '@modules/users/domain/models/ICreateUser';
 import { IUserRepository } from '@modules/users/domain/repositories/IUserRepository';
 import { Repository, getRepository } from 'typeorm';
 import User from '../entities/User';
+import { IPaginateUser } from '@modules/users/domain/models/IPaginateUser';
+
+type SearchParams = {
+  page: number;
+  skip: number;
+  take: number;
+};
 
 class UsersRepository implements IUserRepository {
   private ormRepository: Repository<User>;
   constructor() {
     this.ormRepository = getRepository(User);
+  }
+
+  public async create({ name, email, password }: ICreateUser): Promise<User> {
+    const user = this.ormRepository.create({ name, email, password });
+
+    await this.ormRepository.save(user);
+
+    return user;
+  }
+
+  public async save(user: User): Promise<User> {
+    await this.ormRepository.save(user);
+
+    return user;
+  }
+
+  public async findAll({
+    page,
+    skip,
+    take,
+  }: SearchParams): Promise<IPaginateUser> {
+    const [users, count] = await this.ormRepository
+      .createQueryBuilder()
+      .skip(skip)
+      .take(take)
+      .getManyAndCount();
+
+    const result = {
+      per_page: take,
+      total: count,
+      current_page: page,
+      data: users,
+    };
+
+    return result;
   }
   public async findByName(name: string): Promise<User | undefined> {
     const user = await this.ormRepository.findOne({
@@ -28,7 +71,7 @@ class UsersRepository implements IUserRepository {
   }
 
   public async findByEmail(email: string): Promise<User | undefined> {
-    const user = await this..findOne({
+    const user = await this.ormRepository.findOne({
       where: {
         email,
       },
